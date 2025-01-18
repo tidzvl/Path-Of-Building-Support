209b06ccc44c1b84249937fd71842a1ca80e228e
TiDz <nguyentinvs123@Gmail.com>
2025-01-18 23:24:11 +0700

    update api and draw some task

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # Path-Of-Building-Support
+
+## Simple Documentation
+
+### Task 1
+
+- Build the extension have function scan the pob display by mouse move.
+- Per mouse move, item discription change, i take this and throw to list.
+- Per item find by mouse, i find the modified of item in `get list of stat` api.
+- After that, i build the query have all item modified and post to search.
+- The search post is realtime. And display how many item list on trade site.
+- That can build the sort link to go trade site.
+----------------------------------------------------------------
+
+### Task 2
+
+- Detect full gems at gem list. 
+- Per gem, we can get requirements level and who is sell that.
+- [OPTIONAL] if i'm intellisense, i can get token and get level of character realtime. Per level up, i can notifications to user for buy the gem.
+- If i'm not intellisense, i want to build the roadmap of gem for user.
+
+----------------------------------------------------------------
+
+### Task 3
+
+- If it ok, i can build the application have function auto whisper the seller when item i checked is listing.
--- testsearch.js
+++ testsearch.js
@@ -1,4 +1,4 @@
-const targetNode = document.querySelector('div[data-hk="25.0"]');
+const targetNodes = document.querySelectorAll('div.absolute.z-30.pointer-events-none.hidden.overflow-hidden');
 
 const config = { attributes: true, childList: true, subtree: true };
 
@@ -7,14 +7,14 @@ const returnedValues = new Set();
 const callback = function(mutationsList, observer) {
     for(let mutation of mutationsList) {
         if (mutation.type === 'childList' || mutation.type === 'attributes') {
-            const rarity = targetNode.querySelector('[data-rarity]');
-            if (rarity && !returnedValues.has(rarity.getAttribute('data-rarity'))) {
-                console.log(`data-rarity="${rarity.getAttribute('data-rarity')}"`);
-                returnedValues.add(rarity.getAttribute('data-rarity'));
-            }
-
-            const divElements = targetNode.querySelectorAll('div');
-            divElements.forEach(div => {
+            mutation.target.querySelectorAll('[data-rarity]').forEach(rarity => {
+                if (!returnedValues.has(rarity.getAttribute('data-rarity'))) {
+                    console.log(`data-rarity="${rarity.getAttribute('data-rarity')}"`);
+                    returnedValues.add(rarity.getAttribute('data-rarity'));
+                }
+            });
+            
+            mutation.target.querySelectorAll('div').forEach(div => {
                 const divText = div.childNodes.length === 1 && div.childNodes[0].nodeType === Node.TEXT_NODE ? div.textContent : '';
                 if (divText && !returnedValues.has(divText)) {
                     console.log(divText);
@@ -22,8 +22,7 @@ const callback = function(mutationsList, observer) {
                 }
             });
 
-            const ulElements = targetNode.querySelectorAll('ul');
-            ulElements.forEach(ul => {
+            mutation.target.querySelectorAll('ul').forEach(ul => {
                 if (!returnedValues.has(ul.outerHTML)) {
                     console.log(ul.outerHTML);
                     returnedValues.add(ul.outerHTML);
@@ -33,7 +32,12 @@ const callback = function(mutationsList, observer) {
     }
 };
 
-const observer = new MutationObserver(callback);
-observer.observe(targetNode, config);
+targetNodes.forEach(node => {
+    const observer = new MutationObserver(callback);
+    observer.observe(node, config);
+});
 
 // observer.disconnect();
+
+//list of strat: https://www.pathofexile.com/api/trade/data/stats
+//list of item: https://www.pathofexile.com/api/trade/data/items
